ff4186f32d1ff5bc5de4fcdd06b2c9be0f1d0b90
Eleanor Fris <20051577+yambone635@users.noreply.github.com>
2022-03-15 04:40:14 -0500

    Connected Interfaces
     - Connected discord interface to IF interface

--- __init__.py
+++ __init__.py
@@ -8,8 +8,6 @@ from dotenv import load_dotenv
 import discord_interface as di
 
 load_dotenv()
-
 BOT_TOKEN = os.environ["BOT_TOKEN"]
-TEST_SERVER_GID = [int(os.environ["TEST_SERVER_GID"])]
 
-di.start_bot(BOT_TOKEN)#, TEST_SERVER_GID)
+di.start_bot(BOT_TOKEN)
