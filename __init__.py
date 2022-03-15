cc0e901b7cfd8e9795a01cd516426f4d3fb0bc73
Eleanor Fris <20051577+yambone635@users.noreply.github.com>
2022-03-15 02:31:51 -0500

    Moved to Slash Commands
     - Changed the commands over to use slash commands

--- __init__.py
+++ __init__.py
@@ -1,22 +1,50 @@
 """Entry point for the bot client.
 """
 
-import if_interface as ifi
+import sys
+from threading import Thread
+
 import discord_interface as di
 
-import sys
-
 bot_token = ""
+guild_ids = []
 
 if (len(sys.argv) > 1):
     bot_token = sys.argv[1].strip()
-if (not bot_token):
-    bot_token = input ("Enter the bot's token here: ").strip()
+else:
+    bot_token = input("Enter the bot's token here: ").strip()
     print("Thanks! (For future reference, the token can be passed as "
-          + "the command line argument to skip this prompt.)")
+          "the command line argument to skip this prompt.)")
+    
+if (len(sys.argv) > 2):
+    for raw_id in sys.argv[2:]:
+        id = int(raw_id.strip())
+        guild_ids.append(id)
+else:
+    raw_id = input("Enter a guild ID here: ")
+    guild_ids.append(int(raw_id).strip())
+    print("Thanks! (For future reference, the guild IDs can be passed "
+          "as command line arguments after the bot token to skip this "
+          "prompt and/or provide multiple IDs.)")
 
-bot_client = di.start_client(bot_token)
+print(guild_ids)
 
+di.start_bot(bot_token, guild_ids)
+
+"""
+bot_thread = Thread(target = di.start_bot,
+                    args = [bot_token, guild_ids])
+
+# Make sure the thread doesn't prevent us from exiting the program
+bot_thread.daemon = True
+
+bot_thread.start()
+
+while True:
+    command = input().strip()
+    if (command == "quit"):
+        break
+"""
 """
 interpreter = ifi.Interpreter()
 
--- discord_interface.py
+++ discord_interface.py
@@ -2,20 +2,44 @@
 """
 
 import discord
+from discord.ext import commands
 
-_client = discord.Client()
+_bot = discord.Bot()
+_guild_ids = [845735383173169212]
 
-def start_client(bot_token):
-    _client.run(bot_token)
-
-@_client.event
+@_bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(_client))
+    print('We have logged in as {0.user}'.format(_bot))
 
-@_client.event
-async def on_message(message):
-    if message.author == _client.user:
-        return
-    
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+@_bot.slash_command(guild_ids = _guild_ids,
+                    name = "greetings",
+                    description = "Say hi to the bot!")
+async def greetings(ctx):
+    await ctx.respond('Hello!')
+
+def start_bot(bot_token, guild_ids):
+    _guild_ids = guild_ids
+    _bot.run(bot_token)
+
+"""
+class GameManager(commands.Cog):
+    The class that handles managing the games, such as adding or
+    deleting sessions. Does *not* handle user interactions with the
+    games themselves.
+
+    def __init__(self, bot):
+        # TODO: See if this is neccesary
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready():
+        print('We have logged in as {0.user}'.format(_client))
+
+    @commands.slash_command()
+    async def say_hello(message):
+        if message.author == _client.user:
+            return
+        
+        if message.content.startswith('$hello'):
+            await message.channel.send('Hello!')
+"""
